e5c4426800d28d1ba5e7168fc8d0fc4204d84636
Softec Softwares <frankkarlseng@gmail.com>
2025-06-03 09:56:49 +0100

    Update and rename App.jsnano to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Move Vision CRM</h1>
+      <p>Welcome to your CRM dashboard!</p>
+    </div>
+  );
+}
+
+export default App;

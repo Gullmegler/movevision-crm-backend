0d1e7ad2f5c87456dedb18c93b1998f3820d1917
Frank Karlsen <flyttegutta@iMac.Home>
2025-06-03 13:05:40 +0100

    Full setup – build ready

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './index.css'; // Hvis du ikke ønsker styling, lag en tom fil
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />

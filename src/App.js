f04336db2a945f8617232e7f33fdcdff4f24945d
Softec Softwares <frankkarlseng@gmail.com>
2025-06-03 16:55:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from "react";
-import Sidebar from "./components/Sidebar";
-import Calendar from "./components/Calendar";
+import React from 'react';
+import CalendarView from './components/CalendarView';
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="ml-64 w-full">
-        <Calendar />
-      </div>
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-black text-white p-4">
+        <h2 className="text-xl font-bold mb-4">Move Vision CRM</h2>
+        <ul className="space-y-2">
+          <li className="hover:bg-orange-500 p-2 rounded cursor-pointer">Dashboard</li>
+          <li className="hover:bg-orange-500 p-2 rounded cursor-pointer">Calendar</li>
+          <li className="hover:bg-orange-500 p-2 rounded cursor-pointer">Leads</li>
+          <li className="hover:bg-orange-500 p-2 rounded cursor-pointer">Invoices</li>
+          <li className="hover:bg-orange-500 p-2 rounded cursor-pointer">Settings</li>
+        </ul>
+        {/* Profile (bottom) */}
+        <div className="absolute bottom-4 left-4">
+          <img src="/logo.png" alt="Move Vision" className="w-10 h-10 rounded-full mb-1" />
+          <div className="text-sm">Superadmin</div>
+        </div>
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 bg-white p-6">
+        <CalendarView />
+      </main>
     </div>
   );
 }
 
 export default App;
-

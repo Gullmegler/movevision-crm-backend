7530ae19af4c0305fdc14bda670541dd61117d24
Softec Softwares <frankkarlseng@gmail.com>
2025-06-04 09:35:39 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
+git add postcss.config.js
+git commit -m "Fix PostCSS plugin configuration for Tailwind"
+git push origin main

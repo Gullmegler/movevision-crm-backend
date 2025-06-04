d694c7da1d3e670fa66682031aa70ccda97fd714
Softec Softwares <frankkarlseng@gmail.com>
2025-06-04 13:32:48 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+// postcss.config.js
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
+
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-flexbugs-fixes': {}
-  }
-}
+    tailwindcss,
+    autoprefixer,
+    'postcss-flexbugs-fixes': {},
+  },
+};

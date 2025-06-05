f1be9c02e4ce71b5772c417be1462274368657c3
Softec Softwares <frankkarlseng@gmail.com>
2025-06-05 14:59:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Hello Move Vision CRM</h1>
+    <div style={{ padding: '2rem' }}>
+      <h1 style={{ color: 'orange' }}>✅ React funker!</h1>
+      <p>Siden er ikke lenger blank 🎉</p>
     </div>
   );
 }

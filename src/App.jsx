96f2aaeaa492284966b6befbc0f109e8d3edc6e2
Softec Softwares <frankkarlseng@gmail.com>
2025-06-07 10:50:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-const App = () => {
-  return (
-    <div className="text-white">CRM Dashboard kommer her</div>
-  );
-};
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import AppRoutes from './routes';
 
-export default App;
+export default function App() {
+  return (
+    <div className="flex h-screen font-sans text-sm">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Header />
+        <main className="flex-1 bg-gray-50 p-6 overflow-auto">
+          <AppRoutes />
+        </main>
+      </div>
+    </div>
+  );
+}
